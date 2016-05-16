48281ac5dcd129a24a6c2bbe8680fa8b1a020d55
brian <brian.lebiednik@gmail.com>
2016-05-16 16:08:42 -0400

    more work on mongodb with initialization script

--- mongo_test.py
+++ mongo_test.py
@@ -6,3 +6,8 @@ try:
     print "Connected successfully!!!"
 except pymongo.errors.ConnectionFailure, e:
    print "Could not connect to MongoDB: %s" % e
+
+"""
+posts.update({"_id":1234},{"$inc":{"total_posts":9}})
+posts.find_one({"_id":1234})
+"""
---
+++ mongodb_start.sh
@@ -0,0 +1,14 @@
+#!bin/sh
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+sudo apt-get install -y mongodb-org=3.2.6 mongodb-org-server=3.2.6 mongodb-org-shell=3.2.6 mongodb-org-mongos=3.2.6 mongodb-org-tools=3.2.6
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+mongod
+#https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
