88e006586b93c63701b55bd6954ecac3b63e6011
brian <brian.lebiednik@gmail.com>
2016-05-16 14:34:38 -0400

    starting work on mongodb

---
+++ mongo_test.py
@@ -0,0 +1,8 @@
+import pymongo
+
+# Connection to Mongo DB
+try:
+    conn=pymongo.MongoClient()
+    print "Connected successfully!!!"
+except pymongo.errors.ConnectionFailure, e:
+   print "Could not connect to MongoDB: %s" % e
