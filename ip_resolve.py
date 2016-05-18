8628217974e130a4f3a58808cdb292c17e1f6174
brian <brian.lebiednik@gmail.com>
2016-05-18 10:02:51 -0400

    work on resolving IPS

--- ip_resolve.py
+++ ip_resolve.py
@@ -1,11 +1,29 @@
-from pymongo import MongoClient
+import pymongo
 import socket
+import collections
+from ipwhois import IPWhois
+from pprint import pprint
 
-client = MongoClient()
-db = client.dns_ip_db
+try:
+    conn=pymongo.MongoClient()
+    print "Connected successfully!!!"
+except pymongo.errors.ConnectionFailure, e:
+   print "Could not connect to MongoDB: %s" % e
+
+db = conn.dns_ip_db
 url_ip_coll = db.url_ip_coll
+dns_dict = collections.Counter()
 for ip in url_ip_coll.find({'DNS':'Null', 'Attribute': 'PUBLIC'}):
-    try:
-        print socket.gethostbyaddr(str(ip['IP']))
+    dns_dict[ip['IP']] += ip['seen']
+for k, v in dns_dict.most_common(10):
+    print '%s: %i' %(k,v)
+    """try:
+        print socket.gethostbyaddr(str(k))
     except:
         print ip['IP']
+    """#try:
+    obj = IPWhois(str(k))
+    results = obj.lookup_rdap(depth=1)
+    print results['asn_country_code'], results['asn_registry']
+    #except:
+    #    print "IPWhois Failed"
